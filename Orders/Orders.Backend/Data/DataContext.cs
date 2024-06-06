daf5fbc837c34b3deb323c762fc806b082bc51ae
Andres Meneses <andresfit15@gmail.com>
2024-06-05 20:04:22 -0500

    Create database with the first entity countries

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+	public class DataContext : DbContext //Buscar en instalar la ultima version de entity framework para el dbcontext
+	{
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; } //Importa del archivo shared el modelo de country, pluraliza los countries en ingles bien
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+		}
+	}
+}
---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+	public class Country
+	{
+		public int Id { get; set; }
+
+		//Data notations
+		[Display(Name = "País")]
+		[MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+		[Required(ErrorMessage = "El campo {0} es requerido!")]//Data notation para definir que es requerido en la database
+		public string Name { get; set; } = null!; //No puede ser null para evitar el error en name resaltado
+	}
+}
