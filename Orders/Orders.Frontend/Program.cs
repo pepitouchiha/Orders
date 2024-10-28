d3341045ce44b9d2677444231f32a2201316b784
Andres Meneses <andresfit15@gmail.com>
2024-10-28 16:30:04 -0500

    Feat(NavMenu, Index, Program, _imports) se crea la interfaz para los countries y el menú correcto en la navbar.

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -6,8 +6,8 @@ using Orders.Frontend.Repositories;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7272/") });
+																	//Se reemplaza por la url nuestra
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7124/") });
 builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
